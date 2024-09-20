bc3c0fda20b09b0447dc115e2af7674921fae3cb
MortyZhaoy <morty.zhaoy@gmail.com>
2024-09-19 23:20:20 -0700

    update name to boarddogwrt.

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@ set -eu
 # only for debug
 true ${KEEP_CACHE:=1}
 true ${EXTERNAL_ROOTFS_DIR:=}
+true ${NJOBS:=8}
 
-SCRIPTS_DIR=$(cd `dirname $0`; pwd)
-if [ -h $0 ]
-then
+SCRIPTS_DIR=$(
+	cd $(dirname $0)
+	pwd
+)
+if [ -h $0 ]; then
 	CMD=$(readlink $0)
 	SCRIPTS_DIR=$(dirname $CMD)
 fi
@@ -17,40 +20,40 @@ TOP_DIR=$(pwd)
 
 SDFUSE_DIR=$TOP_DIR/scripts/sd-fuse
 # These arrays will be populated in the.mk file
-true ${FRIENDLYWRT_PACKAGE_DIR:=}
-declare -a FRIENDLYWRT_FILES=("")
-declare -a FRIENDLYWRT_PATCHS=("")
+true ${BOARDDOGWRT_PACKAGE_DIR:=}
+declare -a BOARDDOGWRT_FILES=("")
+declare -a BOARDDOGWRT_PATCHS=("")
 
-firsttime_usage()
-{
+PREBUILTS_GCC_AARCH64_PATH="${TOP_DIR}/prebuilts/gcc/linux-x86/aarch64/gcc-arm-11.3-x86_64-aarch64-none-linux-gnu/"
+
+firsttime_usage() {
 	echo ""
 	echo "# select board: "
-	ALL_MK=`find ./device/friendlyelec -type f -name "*.mk" -printf "%f\n"`
+	ALL_MK=$(find ./device/boarddog -type f -name "*.mk" -printf "%f\n")
 	for mk in ${ALL_MK}; do
 		if [ ${mk} != "base.mk" ]; then
 			echo "  ./build.sh $mk"
 		fi
 	done
-	ALL_MK_LINK=`find ./device/friendlyelec -type l -name "*.mk" -printf "%f\n"`
-		for mk in ${ALL_MK_LINK}; do
-			if [ ${mk} != "base.mk" ]; then
-				echo "  ./build.sh $mk"
-			fi
-		done
+	ALL_MK_LINK=$(find ./device/boarddog -type l -name "*.mk" -printf "%f\n")
+	for mk in ${ALL_MK_LINK}; do
+		if [ ${mk} != "base.mk" ]; then
+			echo "  ./build.sh $mk"
+		fi
+	done
 	echo ""
 }
 
-usage()
-{
+usage() {
 	echo "USAGE: ./build.sh <parameter>"
 	firsttime_usage
 	echo "# build module: "
 	echo "  ./build.sh all                -build all"
 	echo "  ./build.sh uboot              -build uboot only"
 	echo "  ./build.sh kernel             -build kernel only"
-	echo "  ./build.sh friendlywrt        -build friendlywrt rootfs only"
+	echo "  ./build.sh boarddogwrt        -build boarddogwrt rootfs only"
 	echo "  ./build.sh sd-img             -pack sd-card image, used to create bootable SD card"
-	echo "  ./build.sh emmc-img           -pack sd-card image, used to write friendlywrt to emmc"
+	echo "  ./build.sh emmc-img           -pack sd-card image, used to write boarddogwrt to emmc"
 	echo "# clean"
 	echo "  ./build.sh clean              -remove old images"
 	echo "  ./build.sh cleanall"
@@ -62,32 +65,29 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-function log_error()
-{
-	local now=`date +%s`
+function log_error() {
+	local now=$(date +%s)
 	printf "\033[1;31m[ERROR]: $*\033[0m \n"
 }
 
-function log_warn()
-{
-	local now=`date +%s`
+function log_warn() {
+	local now=$(date +%s)
 	printf "\033[1;31m[WARN]: $*\033[0m \n"
 }
 
-function log_info()
-{
-	local now=`date +%s`
+function log_info() {
+	local now=$(date +%s)
 	printf "\033[1;32m[INFO]: $* \033[0m \n"
 }
 
-function build_uboot(){
+function build_uboot() {
 	# build uboot
 	echo "============Start building uboot============"
-	echo "SRC				= ${TOP_DIR}/u-boot"
-	echo "TARGET_ARCH		= $TARGET_ARCH"
-	echo "TARGET_PLAT		= $TARGET_PLAT"
-	echo "TARGET_UBOOT_CONFIG=$TARGET_UBOOT_CONFIG"
-	echo "TARGET_OSNAME	 = $TARGET_OSNAME"
+	echo "SRC							= ${TOP_DIR}/u-boot"
+	echo "TARGET_ARCH					= $TARGET_ARCH"
+	echo "TARGET_PLAT					= $TARGET_PLAT"
+	echo "TARGET_UBOOT_CONFIG			= $TARGET_UBOOT_CONFIG"
+	echo "TARGET_OSNAME	 				= $TARGET_OSNAME"
 	echo "========================================="
 
 	if uname -mpi | grep aarch64 >/dev/null; then
@@ -96,7 +96,7 @@ function build_uboot(){
 	fi
 
 	(cd ${SDFUSE_DIR} && {
-		DISABLE_MKIMG=1 UBOOT_SRC=${TOP_DIR}/u-boot ./build-uboot.sh ${TARGET_OSNAME}
+		DISABLE_MKIMG=1 UBOOT_SRC=${TOP_DIR}/u-boot UBOOT_JOBS=$NJOBS ./build-uboot.sh ${TARGET_OSNAME}
 	})
 
 	if [ $? -eq 0 ]; then
@@ -107,18 +107,18 @@ function build_uboot(){
 	fi
 }
 
-function build_kernel(){
+function build_kernel() {
 	# build kernel
 	echo "============Start building kernel============"
-	echo "SRC				  = ${TOP_DIR}/kernel"
-	echo "TARGET_ARCH		  = $TARGET_ARCH"
-	echo "TARGET_PLAT		  = $TARGET_PLAT"
-	echo "TARGET_KERNEL_CONFIG = $TARGET_KERNEL_CONFIG"
-	echo "TARGET_OSNAME		= $TARGET_OSNAME"
+	echo "SRC				  			= ${TOP_DIR}/kernel"
+	echo "TARGET_ARCH		  			= $TARGET_ARCH"
+	echo "TARGET_PLAT		  			= $TARGET_PLAT"
+	echo "TARGET_KERNEL_CONFIG 			= $TARGET_KERNEL_CONFIG"
+	echo "TARGET_OSNAME					= $TARGET_OSNAME"
 	echo "=========================================="
-
+	# cpu number CCPU=$(nproc)
 	(cd ${SDFUSE_DIR} && {
-		DISABLE_MKIMG=1 KCFG="${TARGET_KERNEL_CONFIG}" KERNEL_SRC=${TOP_DIR}/kernel ./build-kernel.sh ${TARGET_OSNAME}
+		DISABLE_MKIMG=1 KCFG="${TARGET_KERNEL_CONFIG}" KERNEL_SRC=${TOP_DIR}/kernel KERNEL_JOBS=$NJOBS ./build-kernel.sh ${TARGET_OSNAME}
 	})
 
 	if [ $? -eq 0 ]; then
@@ -129,40 +129,39 @@ function build_kernel(){
 	fi
 }
 
-function build_friendlywrt(){
-	# build friendlywrt
-	echo "==========Start build friendlywrt=========="
-	echo "TARGET_FRIENDLYWRT_CONFIG=$TARGET_FRIENDLYWRT_CONFIG"
-	echo "FRIENDLYWRT_SRC=$FRIENDLYWRT_SRC"
-	echo "TARGET_PLAT=$TARGET_PLAT"
+function build_boarddogwrt() {
+	# build boarddogwrt
+	echo "==========Start build boarddogwrt=========="
+	echo "TARGET_BOARDDOGWRT_CONFIG		= $TARGET_BOARDDOGWRT_CONFIG"
+	echo "BOARDDOGWRT_SRC				= $BOARDDOGWRT_SRC"
+	echo "TARGET_PLAT					= $TARGET_PLAT"
 	echo "========================================="
 
-	(cd ${FRIENDLYWRT_SRC} && {
+	(cd ${BOARDDOGWRT_SRC} && {
 		./scripts/feeds update -a && ./scripts/feeds install -a
 		if [ $? -ne 0 ]; then
-			echo "====Building friendlywrt failed!===="
+			echo "====Building boarddogwrt failed!===="
 			exit 1
 		fi
 	})
 
-	(cd ${TOP_DIR}/${FRIENDLYWRT_SRC} && {
-		for (( i=0; i<${#FRIENDLYWRT_PATCHS[@]}; i++ ));
-		do
-			if [ ! -z ${FRIENDLYWRT_PATCHS[$i]} ]; then
+	(cd ${TOP_DIR}/${BOARDDOGWRT_SRC} && {
+		for ((i = 0; i < ${#BOARDDOGWRT_PATCHS[@]}; i++)); do
+			if [ ! -z ${BOARDDOGWRT_PATCHS[$i]} ]; then
 				OLD_IFS="$IFS"
 				IFS=";"
-				ARR=(${FRIENDLYWRT_PATCHS[$i]})
+				ARR=(${BOARDDOGWRT_PATCHS[$i]})
 				IFS="$OLD_IFS"
 				if [ ${#ARR[@]} -eq 1 ]; then
-					# apply patch to friendlywrt root dir
-					log_info "Applying ${FRIENDLYWRT_PATCHS[$i]} to ${FRIENDLYWRT_SRC}"
-					git am -3 ${TOP_DIR}/${FRIENDLYWRT_PATCHS[$i]}
+					# apply patch to boarddogwrt root dir
+					log_info "Applying ${BOARDDOGWRT_PATCHS[$i]} to ${BOARDDOGWRT_SRC}"
+					git am -3 ${TOP_DIR}/${BOARDDOGWRT_PATCHS[$i]}
 				elif [ ${#ARR[@]} -eq 2 ]; then
 					# apply patch to sub dir
 					if [ -d ${TOP_DIR}/${ARR[1]} ]; then
 						(cd ${TOP_DIR}/${ARR[1]} && {
 							PATCHS=$(ls)
-							cd ${TOP_DIR}/${FRIENDLYWRT_SRC}/${ARR[0]}
+							cd ${TOP_DIR}/${BOARDDOGWRT_SRC}/${ARR[0]}
 							for FILE in ${PATCHS}; do
 								log_info "Applying ${FILE} to ${PWD}"
 								git am -3 ${TOP_DIR}/${ARR[1]}/${FILE}
@@ -170,25 +169,25 @@ function build_friendlywrt(){
 						})
 					else
 						(cd ${ARR[0]} && {
-							log_info "Applying ${ARR[1]} to ${FRIENDLYWRT_SRC}/${ARR[0]}"
+							log_info "Applying ${ARR[1]} to ${BOARDDOGWRT_SRC}/${ARR[0]}"
 							git am -3 ${TOP_DIR}/${ARR[1]}
 						})
 					fi
 				else
-					echo "failed to apply patch: ${FRIENDLYWRT_PATCHS[$i]}, wrong format, please check it."
+					echo "failed to apply patch: ${BOARDDOGWRT_PATCHS[$i]}, wrong format, please check it."
 				fi
 			fi
 		done
 	})
 
 	true ${DEBUG_DOT_CONFIG:=0}
-	$SCRIPTS_DIR/mk-friendlywrt.sh $TARGET_FRIENDLYWRT_CONFIG $FRIENDLYWRT_SRC $TARGET_PLAT
+	WRT_JOBS=$NJOBS $SCRIPTS_DIR/mk-boarddogwrt.sh $TARGET_BOARDDOGWRT_CONFIG $BOARDDOGWRT_SRC $TARGET_PLAT
 	if [ $? -eq 0 ]; then
 		if [ $DEBUG_DOT_CONFIG -eq 0 ]; then
-			echo "====Building friendlywrt ok!===="
+			echo "====Building boarddogwrt ok!===="
 		fi
 	else
-		echo "====Building friendlywrt failed!===="
+		echo "====Building boarddogwrt failed!===="
 		exit 1
 	fi
 }
@@ -196,11 +195,11 @@ function build_friendlywrt(){
 function build_all() {
 	build_uboot
 	build_kernel
-	build_friendlywrt
+	build_boarddogwrt
 	build_sdimg
 }
 
-function clean_old_images(){
+function clean_old_images() {
 	(cd $TOP_DIR/out && {
 		rm -f *.img
 		rm -f *.7z
@@ -210,15 +209,15 @@ function clean_old_images(){
 	})
 }
 
-function clean_all(){
-	echo "clean uboot, kernel, friendlywrt, img files"
+function clean_all() {
+	echo "clean uboot, kernel, boarddogwrt, img files"
 	cd $TOP_DIR/u-boot/ && make distclean && cd -
 	cd $TOP_DIR/kernel && make distclean && cd -
-	cd $TOP_DIR/friendlywrt && make clean && cd -
+	cd $TOP_DIR/boarddogwrt && make clean && cd -
 	cd ${SDFUSE_DIR} && ./clean.sh && cd -
 }
 
-function copy_and_verify(){
+function copy_and_verify() {
 	if [ ! -f $1 ]; then
 		echo "not found: $1"
 		echo "$3"
@@ -227,7 +226,7 @@ function copy_and_verify(){
 	cp $1 $2
 }
 
-function prepare_image_for_friendlyelec_eflasher(){
+function prepare_image_for_friendlyelec_eflasher() {
 	local OS_DIR=$1
 	local ROOTFS=$2
 	if [ ! -d ${SDFUSE_DIR}/${OS_DIR} ]; then
@@ -243,37 +242,36 @@ function prepare_image_for_friendlyelec_eflasher(){
 		rm -rf ${SDFUSE_DIR}/out/rootfs.*
 		ROOTFS_DIR=$(mktemp -d ${SDFUSE_DIR}/out/rootfs.XXXXXXXXX)
 	fi
-	log_info "Copying ${TOP_DIR}/${FRIENDLYWRT_SRC}/${FRIENDLYWRT_ROOTFS} to ${ROOTFS_DIR}/"
-	cp -af ${TOP_DIR}/${FRIENDLYWRT_SRC}/${FRIENDLYWRT_ROOTFS}/* ${ROOTFS_DIR}/
+	log_info "Copying ${TOP_DIR}/${BOARDDOGWRT_SRC}/${BOARDDOGWRT_ROOTFS} to ${ROOTFS_DIR}/"
+	cp -af ${TOP_DIR}/${BOARDDOGWRT_SRC}/${BOARDDOGWRT_ROOTFS}/* ${ROOTFS_DIR}/
 
-	echo "$(date +%Y%m%d)" > ${ROOTFS_DIR}/etc/rom-version
-	for (( i=0; i<${#FRIENDLYWRT_FILES[@]}; i++ ));
-	do
+	echo "$(date +%Y%m%d)" >${ROOTFS_DIR}/etc/rom-version
+	for ((i = 0; i < ${#BOARDDOGWRT_FILES[@]}; i++)); do
 		# apply patch to rootfs
-		if [ ! -z ${FRIENDLYWRT_FILES[$i]} ]; then
-			log_info "Applying ${FRIENDLYWRT_FILES[$i]} to ${ROOTFS_DIR}"
-			if [ -f ${TOP_DIR}/${FRIENDLYWRT_FILES[$i]}/install.sh ]; then
-				(cd ${TOP_DIR}/${FRIENDLYWRT_FILES[$i]} && {
+		if [ ! -z ${BOARDDOGWRT_FILES[$i]} ]; then
+			log_info "Applying ${BOARDDOGWRT_FILES[$i]} to ${ROOTFS_DIR}"
+			if [ -f ${TOP_DIR}/${BOARDDOGWRT_FILES[$i]}/install.sh ]; then
+				(cd ${TOP_DIR}/${BOARDDOGWRT_FILES[$i]} && {
 					TOP_DIR=${TOP_DIR} ./install.sh ${ROOTFS_DIR}
 				})
 			else
-				rsync -a --no-o --no-g --exclude='.git' ${TOP_DIR}/${FRIENDLYWRT_FILES[$i]}/* ${ROOTFS_DIR}/
+				rsync -a --no-o --no-g --exclude='.git' ${TOP_DIR}/${BOARDDOGWRT_FILES[$i]}/* ${ROOTFS_DIR}/
 			fi
 		fi
 	done
 
 	# Notes:
-	# The following operation must be applied after FRIENDLYWRT_FILES has been applied
-	# 
-	PKG_DIR=${FRIENDLYWRT_PACKAGE_DIR}
+	# The following operation must be applied after BOARDDOGWRT_FILES has been applied
+	#
+	PKG_DIR=${BOARDDOGWRT_PACKAGE_DIR}
 	if [ -z ${PKG_DIR} ]; then
 		log_error "pkg_dir is empty, why?"
 		exit 1
 	else
 		[ -d ${ROOTFS_DIR}/opt ] || mkdir ${ROOTFS_DIR}/opt
-		cp -af ${TOP_DIR}/${FRIENDLYWRT_SRC}/${PKG_DIR} ${ROOTFS_DIR}/opt/
+		cp -af ${TOP_DIR}/${BOARDDOGWRT_SRC}/${PKG_DIR} ${ROOTFS_DIR}/opt/
 		sed -i -e '/file\:\/\/opt\/$(basename ${PKG_DIR})/d' ${ROOTFS_DIR}/etc/opkg/distfeeds.conf
-		echo "src/gz friendlywrt_packages file://opt/$(basename ${PKG_DIR})" >> ${ROOTFS_DIR}/etc/opkg/distfeeds.conf
+		echo "src/gz boarddogwrt_packages file://opt/$(basename ${PKG_DIR})" >>${ROOTFS_DIR}/etc/opkg/distfeeds.conf
 		sed -i '/check_signature/d' ${ROOTFS_DIR}/etc/opkg.conf
 	fi
 
@@ -299,9 +297,9 @@ function prepare_image_for_friendlyelec_eflasher(){
 			return 1
 		fi
 
-		./tools/prepare_friendlywrt_kernelmodules.sh ${ROOTFS_DIR}
+		./tools/prepare_boarddogwrt_kernelmodules.sh ${ROOTFS_DIR}
 		if [ $? -ne 0 ]; then
-			log_error "error: fail to fix kernel module for friendlywrt to rootfs.img."
+			log_error "error: fail to fix kernel module for boarddogwrt to rootfs.img."
 			return 1
 		fi
 
@@ -310,7 +308,7 @@ function prepare_image_for_friendlyelec_eflasher(){
 		if [ $? -ne 0 ]; then
 			log_error "error: fail to gen boot.img."
 			return 1
-		fi 
+		fi
 
 		log_info "prepare rootfs.img ..."
 		./build-rootfs-img.sh ${ROOTFS_DIR} ${OS_DIR} 0
@@ -319,7 +317,7 @@ function prepare_image_for_friendlyelec_eflasher(){
 			return 1
 		fi
 
-		cat > ./${OS_DIR}/info.conf << EOL
+		cat >./${OS_DIR}/info.conf <<EOL
 title=${OS_DIR}
 require-board=${TARGET_PLAT}
 version=$(date +%Y-%m-%d)
@@ -344,15 +342,14 @@ EOL
 		echo "-----------------------------------------"
 		echo "rootfs dir:"
 		echo "	${ROOTFS_DIR}"
-	echo "boot dir:"
-	echo "	${BOOT_DIR}"
-		echo "-----------------------------------------"	
+		echo "boot dir:"
+		echo "	${BOOT_DIR}"
+		echo "-----------------------------------------"
 	fi
 	return 0
 }
 
-function clean_device_files()
-{
+function clean_device_files() {
 	# create tmp dir
 	if [ ! -d ${1}/tmp ]; then
 		mkdir ${1}/tmp
@@ -362,11 +359,11 @@ function clean_device_files()
 	(cd ${1}/dev && find . ! -type d -exec rm {} \;)
 }
 
-function build_sdimg(){
+function build_sdimg() {
 	source ${SDFUSE_DIR}/tools/util.sh
-	local HAS_BUILT_UBOOT=`has_built_uboot ${TOP_DIR}/u-boot ${SDFUSE_DIR}/out`
-	local HAS_BUILD_KERN=`has_built_kernel ${TOP_DIR}/kernel ${SDFUSE_DIR}/out`
-	local HAS_BUILD_KERN_MODULES=`has_built_kernel_modules ${TOP_DIR}/kernel ${SDFUSE_DIR}/out`
+	local HAS_BUILT_UBOOT=$(has_built_uboot ${TOP_DIR}/u-boot ${SDFUSE_DIR}/out)
+	local HAS_BUILD_KERN=$(has_built_kernel ${TOP_DIR}/kernel ${SDFUSE_DIR}/out)
+	local HAS_BUILD_KERN_MODULES=$(has_built_kernel_modules ${TOP_DIR}/kernel ${SDFUSE_DIR}/out)
 
 	# log_info "HAS_BUILT_UBOOT = ${HAS_BUILT_UBOOT}"
 	# log_info "HAS_BUILD_KERN = ${HAS_BUILD_KERN}"
@@ -378,7 +375,7 @@ function build_sdimg(){
 			exit 1
 		fi
 	fi
-	
+
 	if [ ${HAS_BUILD_KERN} -ne 1 ]; then
 		log_error "error: please build kernel first."
 		exit 1
@@ -389,13 +386,13 @@ function build_sdimg(){
 		exit 1
 	fi
 
-	local ROOTFS=${TOP_DIR}/${FRIENDLYWRT_SRC}/${FRIENDLYWRT_ROOTFS}
+	local ROOTFS=${TOP_DIR}/${BOARDDOGWRT_SRC}/${BOARDDOGWRT_ROOTFS}
 	prepare_image_for_friendlyelec_eflasher ${TARGET_IMAGE_DIRNAME} ${ROOTFS} && (cd ${SDFUSE_DIR} && {
-	./mk-sd-image.sh ${TARGET_IMAGE_DIRNAME} ${TARGET_SD_RAW_FILENAME}
+		./mk-sd-image.sh ${TARGET_IMAGE_DIRNAME} ${TARGET_SD_RAW_FILENAME}
 		(cd out && {
-		rm -f ${TARGET_SD_RAW_FILENAME}.gz
-		gzip --keep ${TARGET_SD_RAW_FILENAME}
-	})
+			rm -f ${TARGET_SD_RAW_FILENAME}.gz
+			gzip --keep ${TARGET_SD_RAW_FILENAME}
+		})
 		echo "-----------------------------------------"
 		echo "Run the following command for sdcard install:"
 		echo "	sudo dd if=out/${TARGET_SD_RAW_FILENAME} bs=1M of=/dev/sdX"
@@ -404,24 +401,15 @@ function build_sdimg(){
 }
 
 function install_toolchain() {
-	if [ ! -d /opt/FriendlyARM/toolchain/4.9.3 ]; then
-		log_info "installing toolchain: arm-linux-gcc 4.9.3"
-		sudo su -c "mkdir -p /opt/FriendlyARM/toolchain && cat $TOP_DIR/toolchain/gcc-x64/toolchain-4.9.3-armhf.tar.gz* | sudo tar xz -C /"
-	fi
-
-	if [ ! -d /opt/FriendlyARM/toolchain/6.4-aarch64 ]; then
-		log_info "installing toolchain: aarch-linux-gcc 6.4"
-		sudo su -c "mkdir -p /opt/FriendlyARM/toolchain && cat $TOP_DIR/toolchain/gcc-x64/toolchain-6.4-aarch64.tar.gz* | sudo tar xz -C /"
-	fi
-
-	if [ ! -d /opt/FriendlyARM/toolchain/11.3-aarch64 ]; then
+	if [ ! -d "$PREBUILTS_GCC_AARCH64_PATH" ]; then
+		mkdir -p $PREBUILTS_GCC_AARCH64_PATH
 		log_info "installing toolchain: aarch-linux-gcc 11.3"
-		sudo su -c "mkdir -p /opt/FriendlyARM/toolchain && tar xf $TOP_DIR/toolchain/gcc-x64/toolchain-11.3-aarch64.tar.xz -C /"
+		git clone https://github.com/BoardDogWrt/gcc-arm-11.3-x86_64-aarch64-none-linux-gnu.git -b boarddogwrt --depth 1 "${PREBUILTS_GCC_AARCH64_PATH}"
 	fi
 }
 
 function build_emmcimg() {
-	local ROOTFS=${TOP_DIR}/${FRIENDLYWRT_SRC}/${FRIENDLYWRT_ROOTFS}
+	local ROOTFS=${TOP_DIR}/${BOARDDOGWRT_SRC}/${BOARDDOGWRT_ROOTFS}
 	prepare_image_for_friendlyelec_eflasher ${TARGET_IMAGE_DIRNAME} ${ROOTFS} && (cd ${SDFUSE_DIR} && {
 		# auto download eflasher image
 		if [ ! -f "eflasher/rootfs.img" ]; then
@@ -437,12 +425,10 @@ function build_emmcimg() {
 
 ##############################################
 
-
-
 MK_LINK=".current_config.mk"
-FOUND_MK_FILE=`find device/friendlyelec -name ${1} | wc -l`
+FOUND_MK_FILE=$(find device/boarddog -name ${1} | wc -l)
 if [ $FOUND_MK_FILE -gt 0 ]; then
-	MK_FILE=`ls device/friendlyelec/*/${1}`
+	MK_FILE=$(ls device/boarddog/*/${1})
 	echo "using config ${MK_FILE}"
 	rm -f ${MK_LINK}
 	ln -s ${MK_FILE} ${MK_LINK}
@@ -467,14 +453,14 @@ else
 	#=========================
 	# build target
 	#=========================
-    if [ $BUILD_TARGET == uboot -o $BUILD_TARGET == u-boot ];then
+	if [ $BUILD_TARGET == uboot -o $BUILD_TARGET == u-boot ]; then
 		build_uboot
 		exit 0
-	elif [ $BUILD_TARGET == kernel ];then
+	elif [ $BUILD_TARGET == kernel ]; then
 		build_kernel
 		exit 0
-	elif [ $BUILD_TARGET == friendlywrt ];then
-		build_friendlywrt
+	elif [ $BUILD_TARGET == boarddogwrt ]; then
+		build_boarddogwrt
 		exit 0
 	elif [ $BUILD_TARGET == sd-img ]; then
 		build_sdimg
@@ -482,13 +468,13 @@ else
 	elif [ $BUILD_TARGET == emmc-img ]; then
 		build_emmcimg
 		exit 0
-	elif [ $BUILD_TARGET == all ];then
+	elif [ $BUILD_TARGET == all ]; then
 		build_all
 		exit 0
-	elif [ $BUILD_TARGET == clean ];then
+	elif [ $BUILD_TARGET == clean ]; then
 		clean_old_images
 		exit 0
- 	elif [ $BUILD_TARGET == cleanall ];then
+	elif [ $BUILD_TARGET == cleanall ]; then
 		# Automatically re-run script under sudo if not root
 		if [ $(id -u) -ne 0 ]; then
 			echo "Re-running script under sudo..."
--- mk-boarddogwrt.sh
+++ mk-boarddogwrt.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
-SCRIPTS_DIR=$(cd `dirname $0`; pwd)
-if [ -h $0 ]
-then
+SCRIPTS_DIR=$(
+	cd $(dirname $0)
+	pwd
+)
+if [ -h $0 ]; then
 	CMD=$(readlink $0)
 	SCRIPTS_DIR=$(dirname $CMD)
 fi
@@ -12,29 +14,32 @@ cd $SCRIPTS_DIR
 cd ../
 TOP_DIR=$(pwd)
 
-TARGET_FRIENDLYWRT_CONFIG=$1
-FRIENDLYWRT_SRC_PATHNAME=$2
+TARGET_BOARDDOGWRT_CONFIG=$1
+BOARDDOGWRT_SRC_PATHNAME=$2
 TARGET_PLAT=$3
 
-cd ${TOP_DIR}/${FRIENDLYWRT_SRC_PATHNAME}
+true ${WRT_JOB:=4}
+WRT_JOB=$WRT_JOBS
+
+cd ${TOP_DIR}/${BOARDDOGWRT_SRC_PATHNAME}
 if [ ! -f .config ]; then
-	if [ -d ${TOP_DIR}/configs/${TARGET_FRIENDLYWRT_CONFIG} ]; then
+	if [ -d ${TOP_DIR}/configs/${TARGET_BOARDDOGWRT_CONFIG} ]; then
 		CURRPATH=$PWD
 		readonly CURRPATH
 		touch ${CURRPATH}/.config
-		(cd ${TOP_DIR}/configs/${TARGET_FRIENDLYWRT_CONFIG} && {
+		(cd ${TOP_DIR}/configs/${TARGET_BOARDDOGWRT_CONFIG} && {
 			for FILE in $(ls); do
 				if [ -f ${FILE} ]; then
 					echo "# apply ${FILE} to .config"
-					cat ${FILE} >> ${CURRPATH}/.config
+					cat ${FILE} >>${CURRPATH}/.config
 				fi
 			done
 		})
 	else
-		cp ${TOP_DIR}/configs/${TARGET_FRIENDLYWRT_CONFIG} .config
+		cp ${TOP_DIR}/configs/${TARGET_BOARDDOGWRT_CONFIG} .config
 	fi
-	sed -i -e '/^# CONFIG_PACKAGE_kmod-/d' .config
-	echo CONFIG_ALL_KMODS=y >> .config
+	sed -i -e '/^#  -/d' .config
+	echo CONFIG_ALL_KMODS=y >>.config
 
 	# Fix compilation errors on the arm64 platform
 	if uname -mpi | grep aarch64 >/dev/null; then
@@ -42,7 +47,7 @@ if [ ! -f .config ]; then
 			if grep -q "CONFIG_GOLANG_EXTERNAL_BOOTSTRAP_ROOT" .config; then
 				sed -i '/CONFIG_GOLANG_EXTERNAL_BOOTSTRAP_ROOT/d' .config
 			fi
-			echo "CONFIG_GOLANG_EXTERNAL_BOOTSTRAP_ROOT=\"/usr/lib/go\"" >> .config
+			echo "CONFIG_GOLANG_EXTERNAL_BOOTSTRAP_ROOT=\"/usr/lib/go\"" >>.config
 		fi
 		if [ ! -e /usr/include/asm ]; then
 			sudo ln -s /usr/include/aarch64-linux-gnu/asm /usr/include/asm
@@ -65,11 +70,11 @@ if [ ! -d dl ]; then
 	# cp -af /opt4/openwrt-full-dl ./dl
 	echo "dl directory doesn't  exist. Will make download full package from openwrt site."
 fi
-make download -j$(nproc)
+make download -j$WRT_JOB
 find dl -size -1024c -exec ls -l {} \;
 find dl -size -1024c -exec rm -f {} \;
 
-make -j$(nproc)
+make -j$WRT_JOB
 RET=$?
 if [ $RET -eq 0 ]; then
 	exit 0
